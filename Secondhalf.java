6c315d40e79cc88f47f109864ecd22dd5a7e2afd
SRIJA217 <52095072+SRIJA217@users.noreply.github.com>
2021-10-13 13:40:50 +0530

    Update Secondhalf.java

--- Secondhalf.java
+++ Secondhalf.java
@@ -1,1 +1,12 @@
-
+import java.util.Scanner;
+class FirstHalf 
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a string");
+    String str=sc.nextLine();
+    str=str.substring(0,str.length()/2);
+    System.out.println(str);
+  }
+}
